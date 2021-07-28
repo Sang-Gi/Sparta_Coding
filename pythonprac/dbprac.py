f24f5a9609697a992a186edb978b68507c02b634
SangGi <jackid1103@naver.com>
2021-07-28 17:07:21 +0900

    sparta week4 end.

--- pythonprac/dbprac.py
+++ pythonprac/dbprac.py
@@ -2,7 +2,7 @@
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
-
+#인서트 저장
 doc = {'name':'bobby','age':25}
 db.users.insert_one(doc)
 
@@ -10,9 +10,9 @@ db.users.insert_one(doc)
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
-
-same_ages = list(db.users.find({'age':25},{'_id':False}))
 #리스트 찾기
+same_ages = list(db.users.find({'age':25},{'_id':False}))
+
 for perseon in same_ages:
     print(perseon)
 # %%
